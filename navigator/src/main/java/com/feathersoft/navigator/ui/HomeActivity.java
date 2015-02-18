a9cd28920dce8de71c12315d1920bdd5cedc77fa
Jorge Méndez <jorge.mendez@globant.com>
2015-02-17 20:32:21 -0600

    Initial commit

---
+++ navigator/src/main/java/com/feathersoft/navigator/ui/HomeActivity.java
@@ -0,0 +1,11 @@
+package com.feathersoft.navigator.ui;
+
+import android.support.v7.app.ActionBarActivity;
+
+
+/**
+ * Created by GrzegorzFeathers on 2/17/15.
+ */
+public class HomeActivity extends ActionBarActivity {
+
+}
